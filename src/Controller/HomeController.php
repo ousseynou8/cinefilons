38f842d29201ea8b0664b1050e10aea1c995425a
ousseynou8 <ousseynoufofana15@gmail.com>
2020-09-25 12:10:13 +0200

    add top nouveaute

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,13 +12,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(FilmRepository $filmRepository)
+    public function index(FilmRepository $filmRepository, AllocineApi $allocineApi)
     {
+        $list = $allocineApi->getMovieList();
+        //dd(json_decode($list));
         $data = $filmRepository->findAll();
+        $topNouveaute = $filmRepository->findBy([], ['dateDeSortie' => 'DESC', 'note' => 'DESC'], 10);
         //dump($data['feed']['movie'][0]['production']);
         //dd(json_decode($data));
         return $this->render('home/index.html.twig', [
             'list_films' => $data,
+            'top_nouveaute' => $topNouveaute
         ]);
     }
 
